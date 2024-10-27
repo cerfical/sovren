83d84510d9eec67f84ad72ac562ad91b10d50443
cerfical <138404639+cerfical@users.noreply.github.com>
2024-10-27 13:03:04 +0500

    Clean up headers

--- include/sovren/math/math.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Mat2x2.hpp"
-#include "Mat3x3.hpp"
-#include "Mat4x4.hpp"
-#include "Vec2.hpp"
-#include "Vec3.hpp"
-#include "Vec4.hpp"
-#include "util.hpp"
--- include/sovren/sovren.hpp
+++ include/sovren/sovren.hpp
@@ -4,11 +4,19 @@
 #include "Point2.hpp"
 #include "Size2.hpp"
 
+#include "input/EventHandler.hpp"
 #include "input/Keys.hpp"
 #include "input/MouseButtons.hpp"
 
-#include "log/log.hpp"
-#include "math/math.hpp"
+#include "log/Log.hpp"
+
+#include "math/Mat2x2.hpp"
+#include "math/Mat3x3.hpp"
+#include "math/Mat4x4.hpp"
+#include "math/Vec2.hpp"
+#include "math/Vec3.hpp"
+#include "math/Vec4.hpp"
+#include "math/util.hpp"
 
 #include "pal/EventPump.hpp"
 #include "pal/Window.hpp"
